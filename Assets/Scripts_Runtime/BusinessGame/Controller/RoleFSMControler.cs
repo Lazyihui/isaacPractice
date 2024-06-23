6f5bcfd189373c23ae98df815b66c595fc5cc9c4
包子 <chengbaozi6@qq.com>
2024-06-23 20:24:33 +0800

    <feature>add:转头后的一瞬间 返回到正脸

--- Assets/Scripts_Runtime/BusinessGame/Controller/RoleFSMControler.cs
+++ Assets/Scripts_Runtime/BusinessGame/Controller/RoleFSMControler.cs
@@ -7,35 +7,34 @@ public static class RoleFSMConteoller {
     public static void Tick(GameContext ctx, RoleEntity role, float dt) {
         RoleFSMStatus status = role.status;
 
-        if(status == RoleFSMStatus.Idle) {
+        if (status == RoleFSMStatus.Idle) {
             Idle(ctx, role, dt);
-        } 
+        }
         Any_State(ctx, role, dt);
     }
 
     static void Any_State(GameContext ctx, RoleEntity role, float dt) {
 
         Vector2 face = ctx.moduleInput.faceAxis;
-        
-        if(face.x > 0) {
 
+        if (face.x > 0) {
             role.animatior.Play("right");
-        } else if(face.x < 0) {
-
+        } else if (face.x < 0) {
             role.animatior.Play("left");
-
-        } else if(face.y > 0) {
+        } else if (face.y > 0) {
             role.animatior.Play("back");
-        } else if(face.y < 0) {
-            role.animatior.Play("front");
         }
 
-    
+        if (face.x == 0 && face.y == 0) {
+            role.Enter_Idle();
+        }
+
+
     }
 
 
     static void Idle(GameContext ctx, RoleEntity role, float dt) {
-        if(role.idle_isEntering) {
+        if (role.idle_isEntering) {
             role.idle_isEntering = false;
             role.animatior.Play("front");
         }
